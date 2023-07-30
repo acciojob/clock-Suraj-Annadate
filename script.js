662780a4d447e95732689af1f2d525de85cfbb3c
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-07-30 18:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-function updateTimer() {
-      var timerElement = document.getElementById('timer');
-      var now = new Date();
-
-      // Format the date
-      var date = now.getDate().toString().padStart(2, '0');
-      var month = (now.getMonth() + 1).toString().padStart(2, '0');
-      var year = now.getFullYear();
-      var formattedDate = `${date}/${month}/${year}`;
-
-      // Format the time
-      var hours = now.getHours() % 12;
-      var minutes = now.getMinutes().toString().padStart(2, '0');
-      var seconds = now.getSeconds().toString().padStart(2, '0');
-      var amPm = now.getHours() < 12 ? 'AM' : 'PM';
-      var formattedTime = `${hours}:${minutes}:${seconds} ${amPm}`;
-
-      // Update the timer element
-      timerElement.textContent = `${formattedDate}, ${formattedTime}`;
-    }
-
-    // Update the timer every second
-    setInterval(updateTimer, 1000);
+function show(x,y)
+{
+    let count=0;
+    const t=setInterval(()=>{
+        const a=new Date();
+        const b=a.toLocaleString();
+        document.getElementById('timer').innerHTML = b;
+        count++;
+        if(count===y)
+            clearInterval(t)
+    },x*1000)
+}
+show(1,10)
